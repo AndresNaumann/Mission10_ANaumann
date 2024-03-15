906b39b6e8870535af8e7bb14e53ac9ec0b9f628
Andrew Naumann <andnau702@gmail.com>
2024-03-15 15:09:06 -0600

    stuff

--- Mission10_ANaumann/backend/Mission10_Naumann/obj/Debug/net8.0/Mission10_Naumann.AssemblyInfo.cs
+++ Mission10_ANaumann/backend/Mission10_Naumann/obj/Debug/net8.0/Mission10_Naumann.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission10_Naumann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604d931117896f35a54141a5da576a224a9b588b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission10_Naumann")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission10_Naumann")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
